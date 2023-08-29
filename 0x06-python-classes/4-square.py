e7144c2d425a18e77703f723f67f2575512e71ea
Maryanemwende <maryanemwende333@gmail.com>
2023-08-29 13:15:51 -0700

    4. Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""define a class Square"""
+
+
+class Square:
+    """data representation of class Square"""
+
+    def __init__(self, size=0):
+        """initializing data attributes of Square.
+        size is the size of Square"""
+
+        self.size = size
+    @property
+    def size(self):
+        """retrieves current size of Square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """returns the area of Square"""
+        return self.__size * self.__size
