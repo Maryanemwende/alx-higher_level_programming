76bf2a0905cffa7ea26d5886261b3e1cf73a8cbf
Maryanemwende <maryanemwende333@gmail.com>
2023-08-29 12:09:55 -0700

    2. Size validation fixed

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -11,4 +11,8 @@ class Square:
         self is the parameter to refer to an instance of square,
         size is a data attribute - size of the square."""
 
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
