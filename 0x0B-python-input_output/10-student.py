f5fca2ef70dafba2827396c4f94ac7012232d8dd
Maryanemwende <maryanemwende333@gmail.com>
2023-09-12 03:16:30 -0700

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""a class Student"""
+
+
+class Student:
+    """represents a class - Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes attributes for Student
+        first_name is the first name of student
+        last_name is the last name for student
+        age is the age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        retrieves a dictionary representation of a Student instance
+        """
+        if type(attrs) == list and all(type(i) == str for i in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        else:
+            return self.__dict__
